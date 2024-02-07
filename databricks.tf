d883cb16a5fdbb0555343a3f75b1e72a02267922
Krishna, Neerati G <gopala.krishna@optum.com>
2024-02-07 23:46:56 +0000

    added databricks premium

--- databricks.tf
+++ databricks.tf
@@ -3,7 +3,7 @@ resource "azurerm_databricks_workspace" "example" {
   name                = "ngkdatabrickspoc4517"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  sku                 = "standard"
+  sku                 = "premium"
   managed_services_cmk_key_vault_key_id = azurerm_key_vault_key.generated.id
 
   tags = {
