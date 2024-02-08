e56aac330b371c56e7ba0a472b44cf286435513a
Krishna, Neerati G <gopala.krishna@optum.com>
2024-02-08 20:04:41 +0000

    added more dependencies

--- kv.tf
+++ kv.tf
@@ -31,7 +31,7 @@ resource "azurerm_key_vault_access_policy" "example" {
   key_vault_id = azurerm_key_vault.example.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = "a7bf0a9f-6879-434c-9efa-bc6c5bc1f555"
-
+  depends_on = [ azurerm_key_vault.example ]
   key_permissions = [
       "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy",
       
@@ -46,7 +46,7 @@ resource "azurerm_key_vault_access_policy" "example2" {
   key_vault_id = azurerm_key_vault.example.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = "2ff814a6-3304-4ab8-85cb-cd0e6f879c1d"
-
+  depends_on = [ azurerm_key_vault.example ]
   key_permissions = [
       "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy",
       
