1271b07ff3bea9ee094f29dd6b7b668efd7fd3c7
Krishna, Neerati G <gopala.krishna@optum.com>
2024-02-08 09:29:19 +0000

    removed databricks

--- databricks.tf
+++ databricks.tf
@@ -1,12 +1,12 @@
 
-resource "azurerm_databricks_workspace" "example" {
-  name                = "ngkdatabrickspoc4517"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  sku                 = "premium"
-  managed_services_cmk_key_vault_key_id = azurerm_key_vault_key.generated.id
+# resource "azurerm_databricks_workspace" "example" {
+#   name                = "ngkdatabrickspoc4517"
+#   resource_group_name = azurerm_resource_group.example.name
+#   location            = azurerm_resource_group.example.location
+#   sku                 = "premium"
+#   managed_services_cmk_key_vault_key_id = azurerm_key_vault_key.generated.id
 
-  tags = {
-    Environment = "Production"
-  }
-}
+#   tags = {
+#     Environment = "Production"
+#   }
+# }
