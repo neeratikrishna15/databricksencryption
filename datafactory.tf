d9b298efc670db18870db5ef308756ce39117c0e
Krishna, Neerati G <gopala.krishna@optum.com>
2024-02-08 16:52:41 +0000

    deleted datafactory

--- datafactory.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_data_factory" "example" {
-  name                = "ngkdf451701"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
