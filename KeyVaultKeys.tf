6d44041017e2a99cd3089078060e1c7a4b6e34a8
Krishna, Neerati G <gopala.krishna@optum.com>
2024-02-08 16:30:41 +0000

    depends on

---
+++ KeyVaultKeys.tf
@@ -0,0 +1,83 @@
+# resource "azurerm_key_vault_key" "DatabricksClusterDiskKey" {
+#   name         = "DatabricksClusterDiskKey"
+#   key_vault_id = azurerm_key_vault.databricks_keyvault.id
+#   key_type     = "RSA"
+#   key_size     = 4096
+
+#   rotation_policy {
+#     automatic {
+#       time_before_expiry = "P7D"
+#     }
+
+#     expire_after         = "P1Y"
+#     notify_before_expiry = "P10D"
+#   }
+
+#   key_opts = [
+#     "decrypt",
+#     "encrypt",
+#     "sign",
+#     "unwrapKey",
+#     "verify",
+#     "wrapKey",
+#   ]
+
+#   depends_on = [azurerm_key_vault_access_policy.databricks_appid_keyvault_policy]
+
+# }
+
+# resource "azurerm_key_vault_key" "DatabricksDBFSKey" {
+#   name         = "DatabricksDBFSKey"
+#   key_vault_id = azurerm_key_vault.databricks_keyvault.id
+#   key_type     = "RSA"
+#   key_size     = 4096
+
+#   rotation_policy {
+#     automatic {
+#       time_before_expiry = "P7D"
+#     }
+
+#     expire_after         = "P1Y"
+#     notify_before_expiry = "P10D"
+#   }
+
+#   key_opts = [
+#     "decrypt",
+#     "encrypt",
+#     "sign",
+#     "unwrapKey",
+#     "verify",
+#     "wrapKey",
+#   ]
+
+#   depends_on = [azurerm_key_vault_access_policy.databricks_appid_keyvault_policy]
+
+# }
+
+# resource "azurerm_key_vault_key" "DatabricksManagedServicesKey" {
+#   name         = "DatabricksManagedServicesKey"
+#   key_vault_id = azurerm_key_vault.databricks_keyvault.id
+#   key_type     = "RSA"
+#   key_size     = 4096
+
+#   rotation_policy {
+#     automatic {
+#       time_before_expiry = "P7D"
+#     }
+
+#     expire_after         = "P1Y"
+#     notify_before_expiry = "P10D"
+#   }
+
+#   key_opts = [
+#     "decrypt",
+#     "encrypt",
+#     "sign",
+#     "unwrapKey",
+#     "verify",
+#     "wrapKey",
+#   ]
+
+#   depends_on = [azurerm_key_vault_access_policy.databricks_appid_keyvault_policy]
+
+# }
