52d5f21d342b58d784586237b097a3b58339961b
Krishna, Neerati G <gopala.krishna@optum.com>
2024-02-08 18:48:52 +0000

    added permissions to enterprise databricks id

--- databricks.tf
+++ databricks.tf
@@ -4,8 +4,8 @@ resource "azurerm_databricks_workspace" "example" {
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   sku                 = "premium"
-  # managed_services_cmk_key_vault_key_id = azurerm_key_vault_key.DatabricksManagedServicesKey.id
-  # customer_managed_key_enabled                        = true
+  managed_services_cmk_key_vault_key_id = azurerm_key_vault_key.DatabricksManagedServicesKey.id
+  customer_managed_key_enabled                        = true
   //managed_services_cmk_key_vault_key_id               = azurerm_key_vault_key.DatabricksManagedServicesKey.id
   //managed_disk_cmk_key_vault_key_id                   = azurerm_key_vault_key.DatabricksClusterDiskKey.id
   //managed_disk_cmk_rotation_to_latest_version_enabled = true
