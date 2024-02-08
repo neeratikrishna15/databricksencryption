84352a81a5ee2d147d76fb0ddf99ed12c01f33d2
Krishna, Neerati G <gopala.krishna@optum.com>
2024-02-08 22:58:18 +0000

    databricks dbfs key

--- databricks.tf
+++ databricks.tf
@@ -8,15 +8,15 @@ resource "azurerm_databricks_workspace" "example" {
   
   managed_services_cmk_key_vault_key_id = azurerm_key_vault_key.DatabricksManagedServicesKey.id
   #depends_on = [ azurerm_key_vault_access_policy.example2 ]
-  #managed_services_cmk_key_vault_key_id               = azurerm_key_vault_key.DatabricksManagedServicesKey.id
+  
   managed_disk_cmk_key_vault_key_id                   = azurerm_key_vault_key.DatabricksClusterDiskKey.id
-  #managed_disk_cmk_rotation_to_latest_version_enabled = true
+  managed_disk_cmk_rotation_to_latest_version_enabled = true
   tags = {
     Environment = "Production"
   }
-  managed_disk_cmk_rotation_to_latest_version_enabled = true
+  
 }
-/* resource "azurerm_databricks_workspace_root_dbfs_customer_managed_key" "SSDataBricksManagedServicesKey" {
-  workspace_id     = azurerm_databricks_workspace.SSDataBricks.id
+ resource "azurerm_databricks_workspace_root_dbfs_customer_managed_key" "SSDataBricksManagedServicesKey" {
+  workspace_id     = azurerm_databricks_workspace.example.id
   key_vault_key_id = azurerm_key_vault_key.DatabricksDBFSKey.id
-} */
+} 
