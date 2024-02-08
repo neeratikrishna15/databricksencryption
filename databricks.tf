08849466c3f56f2461521466dc62e0bd7d97a0b4
Krishna, Neerati G <gopala.krishna@optum.com>
2024-02-08 20:46:54 +0000

    added more dependencies on 8

--- databricks.tf
+++ databricks.tf
@@ -7,7 +7,7 @@ resource "azurerm_databricks_workspace" "example" {
   customer_managed_key_enabled                        = true
   
   managed_services_cmk_key_vault_key_id = azurerm_key_vault_key.DatabricksManagedServicesKey.id
-  depends_on = [ azurerm_key_vault_access_policy.example2 ]
+  #depends_on = [ azurerm_key_vault_access_policy.example2 ]
   //managed_services_cmk_key_vault_key_id               = azurerm_key_vault_key.DatabricksManagedServicesKey.id
   //managed_disk_cmk_key_vault_key_id                   = azurerm_key_vault_key.DatabricksClusterDiskKey.id
   #managed_disk_cmk_rotation_to_latest_version_enabled = true
