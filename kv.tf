09183f2d7d55b832ca3e985a945af27e00178095
Krishna, Neerati G <gopala.krishna@optum.com>
2024-02-08 08:41:27 +0000

    added access policy

--- kv.tf
+++ kv.tf
@@ -56,3 +56,17 @@ resource "azurerm_key_vault_key" "generated" {
     notify_before_expiry = "P29D"
   }
 }
+
+resource "azurerm_key_vault_access_policy" "example" {
+  key_vault_id = azurerm_key_vault.example.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = "a7bf0a9f-6879-434c-9efa-bc6c5bc1f555"
+
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+  ]
+}
