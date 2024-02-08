6fab557e0d293c257c12581ef150da06aced52d5
Krishna, Neerati G <gopala.krishna@optum.com>
2024-02-08 22:46:59 +0000

    added databricks provider

--- kv.tf
+++ kv.tf
@@ -41,13 +41,11 @@ resource "azurerm_key_vault_access_policy" "example" {
     "Get",
   ]
 }
-data "azuread_application" "name" {
-  
-}
+
 resource "azurerm_key_vault_access_policy" "example2" {
   key_vault_id = azurerm_key_vault.example.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = "2ff814a6-3304-4ab8-85cb-cd0e6f879c1d"
+  object_id    = "f3680875-c4e1-4d30-8901-37c638dbf98f"
   depends_on = [ azurerm_key_vault.example ]
   key_permissions = [
       "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy",
