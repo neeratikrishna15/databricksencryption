9dfbab8260fbe51df10c2a7924b7ba53678f1cee
Krishna, Neerati G <gopala.krishna@optum.com>
2024-02-09 11:45:37 +0000

    adding the encryption

--- databricks.tf
+++ databricks.tf
@@ -4,13 +4,13 @@ resource "azurerm_databricks_workspace" "example" {
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   sku                 = "premium"
-  customer_managed_key_enabled                        = true
   
+  customer_managed_key_enabled                        = true
   managed_services_cmk_key_vault_key_id = azurerm_key_vault_key.DatabricksManagedServicesKey.id
+  managed_disk_cmk_key_vault_key_id                   = azurerm_key_vault_key.DatabricksClusterDiskKey.id
+  managed_disk_cmk_rotation_to_latest_version_enabled = true
   #depends_on = [ azurerm_key_vault_access_policy.example2 ]
   
-  #managed_disk_cmk_key_vault_key_id                   = azurerm_key_vault_key.DatabricksClusterDiskKey.id
-  #managed_disk_cmk_rotation_to_latest_version_enabled = true
   tags = {
     Environment = "Production"
   }
