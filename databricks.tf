d9b0a50c9d778078b3a2ee1c8b75213c8483c321
Krishna, Neerati G <gopala.krishna@optum.com>
2024-02-10 14:27:02 +0000

    added dependson the databricks id

--- databricks.tf
+++ databricks.tf
@@ -9,8 +9,18 @@ resource "azurerm_databricks_workspace" "example" {
   managed_services_cmk_key_vault_key_id = azurerm_key_vault_key.DatabricksManagedServicesKey.id
   managed_disk_cmk_key_vault_key_id                   = azurerm_key_vault_key.DatabricksClusterDiskKey.id
   managed_disk_cmk_rotation_to_latest_version_enabled = true
-  #depends_on = [ azurerm_key_vault_access_policy.example2 ]
+  depends_on = [ azurerm_key_vault_access_policy.example2 ]
   
+  custom_parameters {
+    storage_account_name                                 = "stdatabricks014517"
+    storage_account_sku_name                             = "Standard_LRS"
+    no_public_ip                                         = true
+    virtual_network_id                                   = data.azurerm_virtual_network.vnet.id
+    public_subnet_name                                   = data.azurerm_subnet.snet_publ01.name
+    private_subnet_name                                  = data.azurerm_subnet.snet_priv01.name
+    public_subnet_network_security_group_association_id  = azurerm_subnet_network_security_group_association.databricks_public.id
+    private_subnet_network_security_group_association_id = azurerm_subnet_network_security_group_association.databricks_private.id
+  }
   tags = {
     Environment = "Production"
   }
