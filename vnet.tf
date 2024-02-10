cc7a196cad9e115e59fe3e6a893ffe338724152b
Krishna, Neerati G <gopala.krishna@optum.com>
2024-02-10 11:54:59 +0000

    added vnet and subnet

--- vnet.tf
+++ vnet.tf
@@ -5,17 +5,6 @@ resource "azurerm_virtual_network" "example" {
   address_space       = ["10.200.0.0/16"]
   dns_servers         = ["10.200.0.4", "10.200.0.5"]
 
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.example.id
-  }
-
   tags = {
     environment = "Production"
   }
