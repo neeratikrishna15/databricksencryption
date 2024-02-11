c06fcb9496dbe44b84159378e60c6fafce3f0385
Krishna, Neerati G <gopala.krishna@optum.com>
2024-02-11 07:54:02 +0000

    adding taint resource

--- kv.tf
+++ kv.tf
@@ -19,7 +19,7 @@ resource "azurerm_key_vault_access_policy" "example" {
   key_vault_id = azurerm_key_vault.example.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = "a7bf0a9f-6879-434c-9efa-bc6c5bc1f555"
-  depends_on = [ azurerm_key_vault.example ]
+  depends_on = [ azurerm_key_vault_access_policy.example4 ]
   key_permissions = [
       "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy",
       
@@ -34,7 +34,7 @@ resource "azurerm_key_vault_access_policy" "example2" {
   key_vault_id = azurerm_key_vault.example.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = "f3680875-c4e1-4d30-8901-37c638dbf98f"
-  depends_on = [ azurerm_key_vault.example ]
+  depends_on = [ azurerm_key_vault_access_policy.example4 ]
   key_permissions = [
       "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy",
       
@@ -49,7 +49,7 @@ resource "azurerm_key_vault_access_policy" "example3" {
   key_vault_id = azurerm_key_vault.example.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = "f12bddc4-d711-4a99-8c5d-d1c28a7457d4"
-  depends_on = [ azurerm_key_vault.example ]
+  depends_on = [ azurerm_key_vault_access_policy.example4 ]
   key_permissions = [
       "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy",
       
@@ -58,6 +58,7 @@ resource "azurerm_key_vault_access_policy" "example3" {
   secret_permissions = [
     "Get","List"
   ]
+  
 }
 
 resource "azurerm_key_vault_access_policy" "example4" {
