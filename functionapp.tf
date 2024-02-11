3d3aa1e2df022e56d82e0c8e041bca2f06ad1d5b
Krishna, Neerati G <gopala.krishna@optum.com>
2024-02-11 08:07:35 +0000

    removed all resources

--- functionapp.tf
+++ functionapp.tf
@@ -2,5 +2,5 @@ resource "azurerm_virtual_network" "example22" {
   name                = "taintnetwork"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  address_space       = ["10.199.0.0/16"]
+  address_space       = ["10.179.0.0/16"]
 }
