ea530bfdbd054600e1c8721810fc2372f0de9a53
neeratikrishna15 <87215801+neeratikrishna15@users.noreply.github.com>
2024-03-14 16:22:49 +0000

    Update file.py

--- file.py
+++ file.py
@@ -1,3 +1,6 @@
+# file.py
+
+import sys
 from base64 import b64encode
 from nacl import encoding, public
 
@@ -7,7 +10,18 @@ def encrypt(public_key: str, secret_value: str) -> str:
     encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
     return b64encode(encrypted).decode("utf-8")
 
+def main(public_key: str, secret_value: str, output_file: str):
+    encrypted_value = encrypt(public_key, secret_value)
+    with open(output_file, "w") as file:
+        file.write(encrypted_value)
 
-encrypted_value = encrypt("N3bJcftDP0/ykmYngCHu6oJsSEnvHa8HBxSRuR1kAGo=", "${{ secrets.TOKEN }}")
-with open("output.txt", "w") as file:
-    file.write(encrypted_value)
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python file.py <public_key> <secret_value> <output_file>")
+        sys.exit(1)
+
+    public_key = sys.argv[1]
+    secret_value = sys.argv[2]
+    output_file = sys.argv[3]
+
+    main(public_key, secret_value, output_file)
