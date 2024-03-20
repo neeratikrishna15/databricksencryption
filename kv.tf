13d614447a2d0a81ba3c6866a5aec75f4d893b3b
neeratikrishna15 <87215801+neeratikrishna15@users.noreply.github.com>
2024-03-20 23:41:32 +0000

    Update kv.tf

--- kv.tf
+++ kv.tf
@@ -1,6 +1,6 @@
 
 
-data "azurerm_client_config" "current" {}
+ data "azurerm_client_config" "current" {}
 
 
 resource "azurerm_key_vault" "example" {
@@ -15,41 +15,26 @@ resource "azurerm_key_vault" "example" {
 }
 
 
-resource "azurerm_key_vault_access_policy" "example" {
-  key_vault_id = azurerm_key_vault.example.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = "a7bf0a9f-6879-434c-9efa-bc6c5bc1f555" #mpkengineer243
-  depends_on = [ azurerm_key_vault.example ]
-  key_permissions = [
-      "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy",
-      
-    ]
-
-  secret_permissions = [
-    "Get",
-  ]
-}
-
-resource "azurerm_key_vault_access_policy" "example2" {
-  key_vault_id = azurerm_key_vault.example.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = "f3680875-c4e1-4d30-8901-37c638dbf98f" #azuredatabricksenterprise
-  depends_on = [ azurerm_key_vault_access_policy.example ]
-  key_permissions = [
-      "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy",
-      
-    ]
-
-  secret_permissions = [
-    "Get","List"
-  ]
-}
-
-# resource "azurerm_key_vault_access_policy" "example3" {
+# resource "azurerm_key_vault_access_policy" "example" {
 #   key_vault_id = azurerm_key_vault.example.id
 #   tenant_id    = data.azurerm_client_config.current.tenant_id
-#   object_id    = "f12bddc4-d711-4a99-8c5d-d1c28a7457d4"
-#   depends_on = [ azurerm_key_vault_access_policy.example4 ]
+#   object_id    = "a7bf0a9f-6879-434c-9efa-bc6c5bc1f555" #mpkengineer243
+#   depends_on = [ azurerm_key_vault.example ]
+#   key_permissions = [
+#       "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy",
+      
+#     ]
+
+#   secret_permissions = [
+#     "Get",
+#   ]
+# }
+
+# resource "azurerm_key_vault_access_policy" "example2" {
+#   key_vault_id = azurerm_key_vault.example.id
+#   tenant_id    = data.azurerm_client_config.current.tenant_id
+#   object_id    = "f3680875-c4e1-4d30-8901-37c638dbf98f" #azuredatabricksenterprise
+#   depends_on = [ azurerm_key_vault_access_policy.example ]
 #   key_permissions = [
 #       "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy",
       
@@ -58,31 +43,47 @@ resource "azurerm_key_vault_access_policy" "example2" {
 #   secret_permissions = [
 #     "Get","List"
 #   ]
-  
 # }
 
-resource "azurerm_key_vault_access_policy" "example4" {
-  key_vault_id = azurerm_key_vault.example.id
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = data.azurerm_client_config.current.object_id
-
-  depends_on = [ azurerm_key_vault.example ]
-  key_permissions = [
-      "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy",
+# # resource "azurerm_key_vault_access_policy" "example3" {
+# #   key_vault_id = azurerm_key_vault.example.id
+# #   tenant_id    = data.azurerm_client_config.current.tenant_id
+# #   object_id    = "f12bddc4-d711-4a99-8c5d-d1c28a7457d4"
+# #   depends_on = [ azurerm_key_vault_access_policy.example4 ]
+# #   key_permissions = [
+# #       "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy",
       
-    ]
+# #     ]
 
-  secret_permissions = [
-    "Get","List"
-  ]
+# #   secret_permissions = [
+# #     "Get","List"
+# #   ]
+  
+# # }
 
-}
+# resource "azurerm_key_vault_access_policy" "example4" {
+#   key_vault_id = azurerm_key_vault.example.id
+#   tenant_id = data.azurerm_client_config.current.tenant_id
+#   object_id = data.azurerm_client_config.current.object_id
+
+#   depends_on = [ azurerm_key_vault.example ]
+#   key_permissions = [
+#       "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy",
+      
+#     ]
+
+#   secret_permissions = [
+#     "Get","List"
+#   ]
+
+# }
 
 
 resource "azurerm_key_vault_access_policy" "example5" {
   key_vault_id = azurerm_key_vault.example.id
   tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = data.azurerm_databricks_workspace.workspac03.storage_account_identity.0.principal_id
+  #object_id = data.azurerm_databricks_workspace.workspac03.storage_account_identity.0.principal_id
+  object_id = module.databricks_workspace_01.output.storage_account_identity[0].principal_id
   depends_on = [ azurerm_key_vault.example ]
   key_permissions = [
       "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy",
@@ -94,17 +95,17 @@ resource "azurerm_key_vault_access_policy" "example5" {
   ]
 }
 
-resource "azurerm_key_vault_access_policy" "example6" {
-  key_vault_id = azurerm_key_vault.example.id
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = data.azurerm_databricks_workspace.workspac03.managed_disk_identity.0.principal_id
-  depends_on = [ azurerm_key_vault.example ]
-  key_permissions = [
-      "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy",
+# resource "azurerm_key_vault_access_policy" "example6" {
+#   key_vault_id = azurerm_key_vault.example.id
+#   tenant_id = data.azurerm_client_config.current.tenant_id
+#   object_id = data.azurerm_databricks_workspace.workspac03.managed_disk_identity.0.principal_id
+#   depends_on = [ azurerm_key_vault.example ]
+#   key_permissions = [
+#       "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy",
       
-    ]
+#     ]
 
-  secret_permissions = [
-    "Get","List"
-  ]
-}
+#   secret_permissions = [
+#     "Get","List"
+#   ]
+# }
